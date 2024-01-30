586a3fe9aa44eb9d8741b322a2d9b4343ac2b0c8
Victor Hugo <torugoestudos@gmail.com>
2024-01-29 22:21:42 -0300

    METODOS HTTP

--- src/index.js
+++ src/index.js
@@ -1,35 +1,48 @@
 const http = require('http');
 const { URL } = require('url');
 
+const bodyParser = require('./helpers/bodyParser');
 const routes = require('./routes');
-
 
 const server = http.createServer((request, response) => {
   const parsedUrl = new URL(`http://localhost:3000${request.url}`)
 
+  let { pathname } = parsedUrl;
+
+  let id = null;
+
+  const splitEndpont = pathname.split('/').filter(Boolean);
   
+  if (splitEndpont.length > 1) {
+    pathname = `/${splitEndpont[0]}/:id`
+    id = splitEndpont[1];
+  }
 
   console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    routeObj.endpoint === pathname && routeObj.method === request.method
   ));
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
-    route.handler(request, response);
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(body))
+    };
+
+    if (['POST', 'PUT'].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response))
+    } else {
+      route.handler(request, response);
+    }
+
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
     response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
   }
-
-  // if (request.url === '/users' && request.method === 'GET') {
-  //   UserController.listUsers(request, response)
-  // } else {
-  //   response.writeHead(404, { 'Content-Type': 'text/html' });
-  //   response.end(`Cannot ${request.method} ${request.url}`)
-  // }
-
   
 });
 
